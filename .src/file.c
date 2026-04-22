4835434013589fd20a5a9825875ed3cfafd61eda
Nhat Khang <khangnguyen123cvn@gmail.con>
2026-04-22 14:07:44 +0700

    prev - amend - modify file.c

--- .src/file.c
+++ .src/file.c
@@ -1,4 +1,6 @@
 //ĐỌC FILE:
+#include <stdio.h>
+#include "../.inc/file.h"
 void hienThiDanhSach(char duongDan[]) 
 {
     FILE *f = fopen(duongDan, "r");
@@ -23,5 +25,32 @@ int main()
     printf("Nhap duong dan file (vd: io/products.dat): ");
     scanf("%s", duongDan);
     hienThiDanhSach(duongDan);
+    return 0;
+}
+//GHI FILE:
+void ghiMoiHoanToan(char duongDan[], char ten[], int sl) 
+{
+    FILE *f = fopen(duongDan, "w");
+    if (f == NULL) 
+    {
+        printf("Loi: Khong tim thay hoac khong mo duoc file!\n");
+        return; 
+    }
+    fprintf(f, "%s %d\n", ten, sl);
+    fclose(f);
+    printf("Da ghi moi san pham: %s thanh cong!\n", ten);
+}
+int main() 
+{
+    char duongDan[100] = "io/products.dat"; 
+    char tenSP[50];
+    int soLuong;
+    printf("--- CHE DO GHI DE (XOA CU) ---\n");
+    printf("Nhap ten san pham: ");
+    scanf("%s", tenSP);
+    printf("Nhap so luong: ");
+    scanf("%d", &soLuong);
+    ghiMoiHoanToan(duongDan, tenSP, soLuong);
+
     return 0;
 }
