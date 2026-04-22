8e76db49bf5c576e330a318121f3b4ec1cc8dd0f
quocalh <anhquocnguyen502@gmail.com>
2026-04-22 16:03:06 +0700

    Thang lon khang, test di len testing.c ma test :sob:

--- .src/file.c
+++ .src/file.c
@@ -1,56 +1,50 @@
-//ĐỌC FILE:
-#include <stdio.h>
+// ĐỌC FILE:
 #include "../.inc/file.h"
-void hienThiDanhSach(char duongDan[]) 
-{
-    FILE *f = fopen(duongDan, "r");
-    if (f == NULL) 
-    {
-        printf("Loi: khong tim thay file tai duong dan: %s\n", duongDan);
-        return;
-    }
-    char ten[50];
-    int sl;
+#include <stdio.h>
+void hienThiDanhSach(char duongDan[]) {
+  FILE *f = fopen(duongDan, "r");
+  if (f == NULL) {
+    printf("Loi: khong tim thay file tai duong dan: %s\n", duongDan);
+    return;
+  }
+  char ten[50];
+  int sl;
 
-    printf("--- DANH SACH KHO HANG ---\n");
-    while (fscanf(f, "%s %d", ten, &sl) == 2) 
-    {
-        printf("San pham: %s | So luong: %d\n", ten, sl);
-    }
-    fclose(f);
+  printf("--- DANH SACH KHO HANG ---\n");
+  while (fscanf(f, "%s %d", ten, &sl) == 2) {
+    printf("San pham: %s | So luong: %d\n", ten, sl);
+  }
+  fclose(f);
 }
-int main() 
-{
-    char duongDan[100];
-    printf("Nhap duong dan file (vd: io/products.dat): ");
-    scanf("%s", duongDan);
-    hienThiDanhSach(duongDan);
-    return 0;
+int main() {
+  char duongDan[100];
+  printf("Nhap duong dan file (vd: io/products.dat): ");
+  scanf("%s", duongDan);
+  hienThiDanhSach(duongDan);
+  return 0;
 }
-//GHI FILE:
-void ghiMoiHoanToan(char duongDan[], char ten[], int sl) 
-{
-    FILE *f = fopen(duongDan, "w");
-    if (f == NULL) 
-    {
-        printf("Loi: Khong tim thay hoac khong mo duoc file!\n");
-        return; 
-    }
-    fprintf(f, "%s %d\n", ten, sl);
-    fclose(f);
-    printf("Da ghi moi san pham: %s thanh cong!\n", ten);
+// GHI FILE:
+void ghiMoiHoanToan(char duongDan[], char ten[], int sl) {
+  FILE *f = fopen(duongDan, "w");
+  if (f == NULL) {
+    printf("Loi: Khong tim thay hoac khong mo duoc file!\n");
+    return;
+  }
+  fprintf(f, "%s %d\n", ten, sl);
+  fclose(f);
+  printf("Da ghi moi san pham: %s thanh cong!\n", ten);
 }
-int main() 
-{
-    char duongDan[100] = "io/products.dat"; 
-    char tenSP[50];
-    int soLuong;
-    printf("--- CHE DO GHI DE (XOA CU) ---\n");
-    printf("Nhap ten san pham: ");
-    scanf("%s", tenSP);
-    printf("Nhap so luong: ");
-    scanf("%d", &soLuong);
-    ghiMoiHoanToan(duongDan, tenSP, soLuong);
-
-    return 0;
-}
+// int main()
+// {
+//     char duongDan[100] = "io/products.dat";
+//     char tenSP[50];
+//     int soLuong;
+//     printf("--- CHE DO GHI DE (XOA CU) ---\n");
+//     printf("Nhap ten san pham: ");
+//     scanf("%s", tenSP);
+//     printf("Nhap so luong: ");
+//     scanf("%d", &soLuong);
+//     ghiMoiHoanToan(duongDan, tenSP, soLuong);
+//
+//     return 0;
+// }
